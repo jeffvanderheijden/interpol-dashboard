0f84c5f1fcdf9e715bf8972b0ef0a15e164403d3
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2024-10-10 13:44:00 +0200

    Fire get request on challenge window load

--- src/components/Challenges/Challenges.js
+++ src/components/Challenges/Challenges.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Challenges.css";
+import { getGroupChallenges } from "../../helpers/data/dataLayer";
 
-const Challenges = () => {
+const Challenges = ({
+    groupId
+}) => {
+    const [challenges, setChallenges] = useState([]);
+
+    useEffect(() => {
+        getGroupChallenges(groupId).then(data => {
+            data.count > 1 ? setChallenges(data) : setChallenges([data]);
+        });
+    }, [groupId]);
+
     return (
         <div>
-            Challenges
+            {challenges.length > 0 && challenges.map(challenge => (
+                <div>{challenge.name}</div>
+            ))}
         </div>
     );
 };
