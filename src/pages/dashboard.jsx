c08372e02a132d7d413424cc44106b3992dfc9ce
Jeff van der Heijden <jheijden@jheijdens-MacBook-Pro.local>
2025-09-07 08:47:47 +0200

    Fixing lost .txt file

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -3,7 +3,6 @@ import { navigate } from "@reach/router";
 import NoSSR from '../components/NoSSR/NoSSR';
 import Window from "../components/Window/Window";
 import { checkSession, getStudentData, getAdditionalStudentData, logout } from "./../helpers/data/dataLayer";
-import Globe from "../components/Globe/GlobeComp";
 import CreateTeam from "../components/Team/CreateTeam";
 import ScoreBoard from "../components/Score/ScoreBoard";
 import Layout from "../components/Layout";
